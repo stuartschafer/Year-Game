2beff57247d64e2159470ee6fcca3da4b16cbe65
Stuart Schafer <stuart_schafer@hotmail.com>
2019-08-08 22:28:44 -0400

    :dolphin: Finished game, now for a few finishing touches likes sounds, etc

--- logic.js
+++ logic.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+    let ending = "";
     let team1Name = "";
     let team2Name = "";
     let firstTeam = "";
@@ -17,11 +18,15 @@ $(document).ready(function() {
     let clap = new Audio("sounds/clap.wav");
     let boo = new Audio("sounds/boo.wav");
     $("#nextQuestion").hide();
+    $("#coolInfo").hide();
+    $("#infoPic").hide();
     $("#guessArea").hide();
+    $(".ansPic").hide();
     $(".arrows").hide();
     $("#checkFinalAnswer").hide();
-    $("#gameOver").hide();
-    let questions = ["What year did the first POTBELLY open?",
+    $("#video").hide();
+    let answerPic = ["bww.jpg", "subway.jpg", "wholefoods.jpg", "tyson.jpg", "hardees.png", "fiveguys.png", "redlobster.jpg", "chipotle.jpg", "ihop.jpg", "chickfila.jpg", "potbelly.jpg"];
+    let questions = ["What year did the first BUFFALO WILD WINGS open?",
     "What year did the first SUBWAY shop open?",
     "What year did the first WHOLE FOODS open?",
     "What year did TYSON FOODS begin?",
@@ -30,9 +35,10 @@ $(document).ready(function() {
     "What year did the first RED LOBSTER open?",
     "What year did the first CHIPOTLE open?",
     "What year did the first IHOP open?",
-    "What year did the first CHICK-FIL-A open?"];
-    let answers = [1977, 1965, 1980, 1935, 1960, 1986, 1968, 1993, 1958, 1967];
-    let solutions = ["A couple who ran a Chicago antique shop one day decided to start selling toasted sandwiches and homemade desserts, and soon enough lines reached  around the block on a daily basis.",
+    "What year did the first CHICK-FIL-A open?",
+    "What year did the first POTBELLY open?"];
+    let answers = [1982, 1965, 1980, 1935, 1960, 1986, 1968, 1993, 1958, 1967, 1977];
+    let solutions = ["BWW was founded in 1982 by business partners Jim Disbrow and Scott Lowery, who fulfilled a need when they couldn't find classic upstate New York-style chicken wings after relocating from Buffalo to Columbus, Ohio.",
     "A recent high school graduate, 17 year old Fred DeLuca, and family friend Dr. Peter Buck team up to open their first submarine sandwich shop in Bridgeport, Connecticut. Originally called “Pete's Super Submarines,” they sell 312 sandwiches the first day.",
     "The first Whole Foods store opened its doors in Austin in September 1980, after John Mackey and Renee Lawson Hardy, owners of the SaferWay health food store, joined forces with Craig Weller and Mark Skiles, owners of Clarksville Natural Grocery.",
     "The history of Tyson Foods started with an Arkansas farmer, John Tyson, who hauled about 50 chickens to sell in Chicago during 1935. He named his business Tyson Feed & Hatchery and began making a profit buying and selling chickens prior to World War II (1939–45).",
@@ -41,8 +47,11 @@ $(document).ready(function() {
     "The first Red Lobster restaurant was opened on January 18, 1968, in Lakeland, Florida, by entrepreneurs Bill Darden and Charley Woodsby.",
     "In 1993, Ells took what he learned in San Francisco and opened the first Chipotle Mexican Grill in Denver, Colorado, in a former Dolly Madison Ice Cream store at 1644 East Evans Avenue, near the University of Denver campus, using an $85,000 loan from his father.",
     "Jerry Lapin, Al Lapin, and Albert Kallis founded International House of Pancakes in 1958 with the help of Sherwood Rosenberg and William Kaye. The first restaurant opened in Toluca Lake, Los Angeles, California.",
-    "On Nov. 24, 1967, Truett Cathy altered the “fast food” landscape forever when the first Chick-fil-A restaurant opened in Atlanta’s Greenbriar Mall, an idea as original as his Chicken Sandwich."];
+    "On Nov. 24, 1967, Truett Cathy altered the “fast food” landscape forever when the first Chick-fil-A restaurant opened in Atlanta’s Greenbriar Mall, an idea as original as his Chicken Sandwich.",
+    "A couple who ran a Chicago antique shop one day decided to start selling toasted sandwiches and homemade desserts, and soon enough lines reached  around the block on a daily basis."];
     
+
+
     // This starts the beginning of the game when the "Flip to see who goes first" is clicked
     $("#beginGame").click(function() {
         $("#teamName").empty();
@@ -83,9 +92,33 @@ $(document).ready(function() {
     });
 
     // This brings up the next question
-    $("#nextQuestion").click(function() {
-        $(".infoSection").empty();
+    $(".nextQues").click(function() {
+        if (ending == "Yes") {
+            $(".teamArea").show();
+            $("#timer").empty();
+            $("#timer").show();
+            $(".ansPic").hide();
+            $("#infoPic").hide();
+            $(".infoSection").empty();
+            $("#nextQuestion").hide();
+            gameOver();
+        } else {
+            startNextQuestion();
+        }
+    });
+
+    function startNextQuestion() {
+        if (ending == "Yes") {
+            gameOver();
+        }
+        $(".teamArea").show();
+        $("#nextQuestion").show();
+        $("#questionArea").show();
         $("#timer").empty();
+        $("#timer").show();
+        $(".ansPic").hide();
+        $("#infoPic").hide();
+        $(".infoSection").empty();
         $("#nextQuestion").hide();
         $("#questionNumber").show();
         $(".teamArea").removeClass("pulse");
@@ -108,16 +141,16 @@ $(document).ready(function() {
         }
         if (x === 2) {
             time = 3600;
-            //time = 300;
+            //time = 3000;
         } else {
             time = 3300;
-            //time = 600;
+            //time = 3000;
         }
         x++;
         $("#questionArea").html(questions[x]);
         timeframe = "firstGuess";
         timer();
-    });
+    }
 
     // This only shows when the second team has to guess before or after AND they select before
     $("#upArrow").click(function() {
@@ -147,7 +180,7 @@ $(document).ready(function() {
         $("#teamName").empty();
         $("#guessArea").hide();
 
-        if (guess < 1900 || guess > 2021 || guessStr === "") {
+        if (guess < 0 || guess > 2021 || guessStr === "") {
             boo.play();
             $("#teamName").html("Please guess an actual year");
             $("#guess").val("");
@@ -169,18 +202,28 @@ $(document).ready(function() {
         $("#questionNumber").empty();
         $("#checkFinalAnswer").hide();
         $("#message").show();
-        $("#questionArea").html(solutions[x]);
+        //$("#questionArea").html(solutions[x]);
         $("#teamGuess").show();
         awardPoint();
     });
 
-    $("#gameOver").click(function() {
-        gameOver();
+
+    $("#coolInfo").click(function() {
+        $("#coolInfo").hide();
+        $(".teamArea").hide();
+        $("#nextQuestion").hide();
+        $("#questionArea").hide();
+        $("#timer").hide();
+        $("#infoPic").show();
+        $("#infoPic").html(solutions[x]);
+        $(".ansPic").attr("src", "images/" + answerPic[x]);
+        $(".ansPic").show();
     });
 
     // This runs to see who wins a point
     function awardPoint() {
         clap.play();
+        $("#questionNumber").empty();
         let otherGuess = "";
         let correctGuess = "";
         if (arrowGuess === "BEFORE") {
@@ -238,17 +281,23 @@ $(document).ready(function() {
         $("#timer").css("color", "white");
         
         // This checks to see if the game is over
-        if ((x >= 5) && (team1Score === team2Score)) {
-            $("#timer").append("<br>WE HAVE A TIE, so 1 more question for a tie-breaker!!! <br>" + nextTeam + " will go first");
-            $("#nextQuestion").show();
-        } else if ((x >= 5) && (team1Score || team2Score)) {
-            $("#gameOver").show();
+        if ((x == 9) && (team1Score === team2Score)) {
+            $("#timer").append("<br><br><span id='tie'>WE HAVE A TIE,<br>so 1 more question for a tie-breaker!!! </span><br>" + nextTeam + " will go first");
+            showPicAndInfo();
+        } else if ((x >= 9) && (team1Score || team2Score)) {
+            $("#coolInfo").show();
+            ending = "Yes";
+            $("#timer").append("<br><br><span id='tie'>That concludes the game.<br>Now to Award the winner.</span><br>");
         } else {
             $("#timer").append("<br><br><span id='guessNext'>" + nextTeam + " will be guessing first for the next round.</span>");
-            $("#nextQuestion").show();
+            showPicAndInfo();
         }
 
-    } 
+    }
+
+    function showPicAndInfo() {
+        $("#coolInfo").show();
+    }
 
     // This is for the count-down timer
     function timer() {
@@ -324,6 +373,7 @@ $(document).ready(function() {
     }
 
     function gameOver() {
+        $("#video").show();
         if (team1Score > team2Score) {
             $("#timer").html("Congradulations " + team1Name + "!, you have defeated " + team2Name);
         } else {
